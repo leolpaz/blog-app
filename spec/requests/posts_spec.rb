8ac47c671c3b10147521d1d651fe0e37d5bf785c
leolpaz <leonardolpaz95@gmail.com>
2022-01-12 12:36:41 -0300

    Write post and user tests

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    before(:each) { get("/users/posts") }
+    it "Gets an ok http status response" do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+    it 'includes the right placeholder' do
+      expect(response.body).to include('Here will be a list of user posts')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) { get("/users/posts/1") }
+
+    it 'render post details correctly' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders the show template' do
+      expect(response).to render_template('show')
+    end
+    it 'includes the right placeholder' do
+      expect(response.body).to include('Here will be a list of a specific user post')
+    end
+  end
+end
