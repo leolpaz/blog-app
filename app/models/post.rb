764915ea508abad8ee01044263343f177ca14082
leolpaz <leonardolpaz95@gmail.com>
2022-01-17 18:51:33 -0300

    Create method to update post counter for user and show recent comments for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+
+  def update_post_counter
+    if user.posts_counter.nil?
+      user.posts_counter = 0
+    end
+    user.posts_counter += 1
+    user.save
+  end
+
+  def recently_commented
+    comments.order(created_at: :desc).limit(5)
+  end
 end
