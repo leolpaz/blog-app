26bffbbdeec00398407d23d3eb083d6527bc406b
leolpaz <leonardolpaz95@gmail.com>
2022-01-17 18:51:51 -0300

    Create method to update like and comment for post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :posts
+
+  def update_comments_counter
+    if post.comments_counter.nil?
+      post.comments_counter = 0
+    end
+    post.comments_counter += 1
+    post.save
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :posts
+
+  def update_likes_counter
+    if post.likes_counter.nil?
+      post.likes_counter = 0
+    end
+    post.likes_counter += 1
+    post.save
+  end
 end
