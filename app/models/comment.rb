25214b5253e5bb7138d6d71a546fa905720c599e
leolpaz <leonardolpaz95@gmail.com>
2022-01-18 13:25:23 -0300

    Fix comment and like controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :posts
+  belongs_to :post
 
   def update_comments_counter
     post.comments_counter = 0 if post.comments_counter.nil?
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :posts
+  belongs_to :post
 
   def update_likes_counter
     post.likes_counter = 0 if post.likes_counter.nil?
