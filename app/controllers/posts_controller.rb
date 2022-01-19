8b377c2fdc3b1b9568dd960bbd0cac7cf0ee9653
leolpaz <leonardolpaz95@gmail.com>
2022-01-19 14:54:57 -0300

    Create methods and routes so the user can create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    current_user = User.find(params[:user_id])
+    @post = current_user.posts.new(post_params.merge(user_id: current_user.id))
+    if @post.save
+      redirect_to [@post.user, @post]
+    else
+      redirect_to [:new_user_post]
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
