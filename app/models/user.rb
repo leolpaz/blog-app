749af96d7d31eefa4e986241b643462631095de5
leolpaz <leonardolpaz95@gmail.com>
2022-01-20 10:22:42 -0300

    Write user tests and fix a bug with user posts ordering

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than: 0 } 
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 } 
   has_many :posts
   has_many :comments
   has_many :likes
 
   def recently_posted
-    posts.order(:title, created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { @u = User.new(name: 'test', posts_counter: 1) }
+  it 'name should be present' do
+    expect(@u).to be_valid
+    @u.name = nil
+    expect(@u).to_not be_valid
+  end
+  it 'posts_counter should be greater or equal than 0' do
+    expect(@u).to be_valid
+    @u.posts_counter = -1
+    expect(@u).to_not be_valid
+    @u.posts_counter = nil
+    expect(@u).to_not be_valid
+    @u.posts_counter = 0
+    expect(@u).to be_valid
+  end
+  it 'correctly shows the 3 most recent posts' do
+    @u.posts.new(title: 'test1', comments_counter: 0, likes_counter: 0)
+    @u.save
+    @u.posts.new(title: 'test2', comments_counter: 0, likes_counter: 0)
+    @u.save
+    @u.posts.new(title: 'test3', comments_counter: 0, likes_counter: 0)
+    @u.save
+    @u.posts.new(title: 'test4', comments_counter: 0, likes_counter: 0)
+    @u.save
+    expect(@u.recently_posted[0].title).to eq 'test4'
+    expect(@u.recently_posted[1].title).to eq 'test3'
+    expect(@u.recently_posted[2].title).to eq 'test2'
+  end
+
+
+
 end
