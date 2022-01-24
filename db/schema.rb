feac107c63d0c51e8cecf73238de66953dadc1b6
brianorora <brianorora@gmail.com>
2022-01-24 17:20:56 +0300

    Add confirmation email functionality

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(version: 2022_01_24_131634) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
+    t.string "confirmation_token"
+    t.datetime "confirmed_at", precision: 6
+    t.datetime "confirmation_sent_at", precision: 6
+    t.string "unconfirmed_email"
     t.index ["address"], name: "index_users_on_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
